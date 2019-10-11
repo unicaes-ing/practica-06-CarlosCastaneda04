092bf56f33a769aab32cd587649665d04afca465
CarlosCastaneda04 <54734121+CarlosCastaneda04@users.noreply.github.com>
2019-10-10 23:46:35 -0600

    Add files via upload

---
+++ practica6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica6
+{
+    class Program
+    {
+        public static void Tabla(int num)
+        {
+            Console.WriteLine("Tabla de Multliplicacion: ");
+            Console.WriteLine();
+            for (int i = 1; i < 13; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+            }
+        }
+        public static void Tabla(int num, ConsoleColor texto)
+        {
+            Console.WriteLine("Tabla de Multliplicacion: ");
+            Console.WriteLine();
+            for (int i = 1; i < 13; i++)
+            {
+                Console.ForegroundColor = texto;
+                Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int nume;
+            Console.WriteLine("Ingrese el numero a multlipicar: ");
+            nume = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Tabla(nume);
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Ingrese el numero a multlipicar: ");
+            nume = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Tabla(nume, ConsoleColor.Red);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ practica6/ejercicio3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica6
+{
+    class ejercicio3
+    {
+        static int BaseXExponente(int baseX, int expo)
+        {
+            if (expo == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return baseX * BaseXExponente(baseX, expo - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int baseX, expo;
+            Console.WriteLine("Ingrese la base: ");
+            baseX = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el exponente: ");
+            expo = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} x {1} = {2}", baseX, expo, BaseXExponente(baseX, expo));
+            Console.ReadKey();
+        }
+    }
+}
